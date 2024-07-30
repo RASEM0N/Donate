4a038905f8ad25ef77402cf66b28bf04cf634ec1
Роман <negativ12fly@gmail.com>
2024-07-31 02:05:56 +0500

    feat: структура проекта

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DonateModule } from './modules/donate/donate.module';
+import { MessagesModule } from './modules/messages/messages.module';
+import { PayServiceModule } from './modules/payService/payservice.module';
 
 @Module({
-  imports: [],
+  imports: [DonateModule, MessagesModule, PayServiceModule],
   controllers: [AppController],
   providers: [AppService],
 })
